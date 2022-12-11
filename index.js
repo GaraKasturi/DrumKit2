286dc0cf15c886bb27d26f296c4361fad049aebe
kasturi gara <garakasturi@gmail.com>
2022-12-11 11:50:30 +0530

    add commit

--- index.js
+++ index.js
@@ -24,28 +24,28 @@ InstrumentSound(event.key);
 
 function InstrumentSound(keyPressed) {
     if (keyPressed==="w"){
-        let sound1 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/crash.mp3");
+        let sound1 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/crash.mp3");
 	    sound1.play();
     }
-    else if (keyPressed==="d"){
-        let sound2 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/kick-bass.mp3");
+    else if (keyPressed==="a"){
+        let sound2 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/kick-bass.mp3");
 	    sound2.play();
     }
     else if (keyPressed==="s"){
-        let sound3 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/snare.mp3');
+        let sound3 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/snare.mp3');
 	    sound3.play();
     }
     else if (keyPressed==="d"){
-        let sound4 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-1.mp3');
+        let sound4 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-1.mp3');
 	    sound4.play();
     }
     else if (keyPressed==="j"){
-        let sound5 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-2.mp3');
+        let sound5 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-2.mp3');
 	    sound5.play();
     }
     else if (keyPressed==="k"){
        
-        let sound6 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-3.mp3');
+        let sound6 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-2.mp3');
         sound6.play();
 	
 
@@ -53,7 +53,7 @@ function InstrumentSound(keyPressed) {
     }
     else if (keyPressed==="l"){
         
-        let sound7 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-4.mp3');
+        let sound7 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-4.mp3');
         sound7.play();
     }
     
@@ -67,28 +67,28 @@ document.addEventListener("click", function(event) {
 
 function InstrumentButtonSound(buttonPressed) {
     if (buttonPressed==="crash"){
-        let sound1 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/crash.mp3");
+        let sound1 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/crash.mp3");
 	    sound1.play();
     }
     else if (buttonPressed==="kick"){
-        let sound2 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/kick-bass.mp3");
+        let sound2 = new Audio("C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/kick-bass.mp3");
 	    sound2.play();
     }
     else if (buttonPressed==="snare"){
-        let sound3 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/snare.mp3');
+        let sound3 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/snare.mp3');
 	    sound3.play();
     }
     else if (buttonPressed==="tom-1"){
-        let sound4 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-1.mp3');
+        let sound4 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-1.mp3');
 	    sound4.play();
     }
     else if (buttonPressed==="tom-2"){
-        let sound5 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-2.mp3');
+        let sound5 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-2.mp3');
 	    sound5.play();
     }
     else if (buttonPressed==="tom-3"){
        
-        let sound6 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-3.mp3');
+        let sound6 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-2.mp3');
         sound6.play();
 	
 
@@ -96,7 +96,7 @@ function InstrumentButtonSound(buttonPressed) {
     }
     else if (buttonPressed==="tom-4"){
         
-        let sound7 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files (1).zip/Drum Kit Starting Files/sounds/tom-4.mp3');
+        let sound7 = new Audio('C:/Users/garak/AppData/Local/Temp/Temp1_Drum Kit Starting Files.zip/Drum Kit Starting Files/sounds/tom-4.mp3');
         sound7.play();
     }
     
